99c76b44dee8bde66c906e2a266b826d011f319f
Cort Buffington <n0mjs@me.com>
2020-02-09 16:33:46 -0600

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -174,7 +174,6 @@ def stream_trimmer_loop():
     _now = time()
 
     for system in systems:
-        print(systems[system].STATUS)
         # HBP systems, master and peer
         if CONFIG['SYSTEMS'][system]['MODE'] != 'OPENBRIDGE':
             for slot in range(1,3):
@@ -746,20 +745,20 @@ class routerHBP(HBSYSTEM):
 
                                     # Is this a new call stream?
                                     if (_stream_id != self.STATUS[_slot]['RX_STREAM_ID']):
-                                         # Record the DST TGID and Stream ID
-                                         _target_status[_target['TS']]['TX_START'] = pkt_time
-                                         _target_status[_target['TS']]['TX_TGID'] = _target['TGID']
-                                         _target_status[_target['TS']]['TX_STREAM_ID'] = _stream_id
-                                         _target_status[_target['TS']]['TX_RFS'] = _rf_src
-                                         _target_status[_target['TS']]['TX_PEER'] = _peer_id
-                                         # Generate LCs (full and EMB) for the TX stream
-                                         dst_lc = self.STATUS[_slot]['RX_LC'][0:3] + _target['TGID'] + _rf_src
-                                         _target_status[_target['TS']]['TX_H_LC'] = bptc.encode_header_lc(dst_lc)
-                                         _target_status[_target['TS']]['TX_T_LC'] = bptc.encode_terminator_lc(dst_lc)
-                                         _target_status[_target['TS']]['TX_EMB_LC'] = bptc.encode_emblc(dst_lc)
-                                         logger.debug('(%s) Generating TX FULL and EMB LCs for HomeBrew destination: System: %s, TS: %s, TGID: %s', self._system, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
-                                         logger.info('(%s) Conference Bridge: %s, Call Bridged to HBP System: %s TS: %s, TGID: %s', self._system, _bridge, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
-                                         if CONFIG['REPORTS']['REPORT']:
+                                        # Record the DST TGID and Stream ID
+                                        _target_status[_target['TS']]['TX_START'] = pkt_time
+                                        _target_status[_target['TS']]['TX_TGID'] = _target['TGID']
+                                        _target_status[_target['TS']]['TX_STREAM_ID'] = _stream_id
+                                        _target_status[_target['TS']]['TX_RFS'] = _rf_src
+                                        _target_status[_target['TS']]['TX_PEER'] = _peer_id
+                                        # Generate LCs (full and EMB) for the TX stream
+                                        dst_lc = self.STATUS[_slot]['RX_LC'][0:3] + _target['TGID'] + _rf_src
+                                        _target_status[_target['TS']]['TX_H_LC'] = bptc.encode_header_lc(dst_lc)
+                                        _target_status[_target['TS']]['TX_T_LC'] = bptc.encode_terminator_lc(dst_lc)
+                                        _target_status[_target['TS']]['TX_EMB_LC'] = bptc.encode_emblc(dst_lc)
+                                        logger.debug('(%s) Generating TX FULL and EMB LCs for HomeBrew destination: System: %s, TS: %s, TGID: %s', self._system, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
+                                        logger.info('(%s) Conference Bridge: %s, Call Bridged to HBP System: %s TS: %s, TGID: %s', self._system, _bridge, _target['SYSTEM'], _target['TS'], int_id(_target['TGID']))
+                                        if CONFIG['REPORTS']['REPORT']:
                                             systems[_target['SYSTEM']]._report.send_bridgeEvent('GROUP VOICE,START,TX,{},{},{},{},{},{}'.format(_target['SYSTEM'], int_id(_stream_id), int_id(_peer_id), int_id(_rf_src), _target['TS'], int_id(_target['TGID'])).encode(encoding='utf-8', errors='ignore'))
 
                                     # Set other values for the contention handler to test next time there is a frame to forward
